ba6344e775f653d949c35df39ded8baa9405e0af
Syed Fawwaz Hussain <fazwalrus@gmail.com>
2024-09-04 18:51:35 -0400

    capri: Import firmware from RRBS31.Q1-3-58-19

--- Android.mk
+++ Android.mk
@@ -6,4 +6,22 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),capri)
 
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,6d177206918709bd9a2eacaf85265353a4918cbe)
+$(call add-radio-file-sha1-checked,radio/modem.img,c090595f77956336f8e918b184a013ab453549c0)
+$(call add-radio-file-sha1-checked,radio/abl.img,dc3e32799f3fe30f8853c1ff7d9f806a744c02f6)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,b4b475983db740922304231e776e4873ef44dbbc)
+$(call add-radio-file-sha1-checked,radio/dsp.img,5cbce57892aa5dfcc4b06f7546061bb21dd9390a)
+$(call add-radio-file-sha1-checked,radio/fsg.img,27e24b718dfd37d805a676cee1ce29c7c8f615c0)
+$(call add-radio-file-sha1-checked,radio/hyp.img,a70b8e2ffc9b01f95269cbfa6809e02dbd7261da)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,d0a083ee9ed7d7540252f60ab214c826fa18189e)
+$(call add-radio-file-sha1-checked,radio/logo.img,aeca08e258e7e4bee43e4d609afc65a10372530c)
+$(call add-radio-file-sha1-checked,radio/prov.img,6eda0266984df43f7651d7f21f4c5f376ec1ef38)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,41fec1d81937312c6380275e0ccbf6ba09432331)
+$(call add-radio-file-sha1-checked,radio/rpm.img,2b126dbe227f0a12aa05cb994ef715c7c757d95a)
+$(call add-radio-file-sha1-checked,radio/storsec.img,3c922faad4499427a20b21179b5152110cb3d623)
+$(call add-radio-file-sha1-checked,radio/tz.img,2fb08211ff032938e64cb10cd55944e832432467)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,1d2e36ab0f4e1b11628b662a412740178a900ac7)
+$(call add-radio-file-sha1-checked,radio/xbl.img,1935dee8ee6fb283f0d6372ad24a54ac998d7ab8)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,e935ffee1e6ba49cd9b364ef3fc394a5593e151f)
+
 endif
